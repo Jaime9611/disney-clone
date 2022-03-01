426961a9a77e47a15c9fd12e838dfd76881c3bd3
Jaime9611 <jaimeescobar9611@gmail.com>
2022-03-01 18:19:51 -0500

    Styles added to the categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import { Container, Wrapper } from './Categories.styles';
+
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <img src="/images/viewers-disney.png" alt="disney category"></img>
+      </Wrapper>
+      <Wrapper>
+        <img src="/images/viewers-pixar.png" alt="pixar category"></img>
+      </Wrapper>
+      <Wrapper>
+        <img src="/images/viewers-marvel.png" alt="marvel category"></img>
+      </Wrapper>
+      <Wrapper>
+        <img src="/images/viewers-starwars.png" alt="starwars category"></img>
+      </Wrapper>
+      <Wrapper>
+        <img src="/images/viewers-national.png" alt="national category"></img>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Categories;
