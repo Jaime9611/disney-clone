94b3f3f5244a4509ec753e3cf579d22ddc794e8b
Jaime9611 <jaimeescobar9611@gmail.com>
2022-03-01 18:00:37 -0500

    ImgSlider component files created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
+import { Home } from './pages';
 import Layout from './pages/Layout';
 import Detail from './pages/Detail';
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './Home.styles';
+import { ImgSlider } from '../../components';
+
+const Home = () => {
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  );
+};
+
+export default Home;
