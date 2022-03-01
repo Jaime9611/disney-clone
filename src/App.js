75917fd9ccd721acd60d92f463d900b2b3c0a2d1
Jaime9611 <jaimeescobar9611@gmail.com>
2022-03-01 16:08:45 -0500

    Added pages, components and React Router :rocket:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from 'react';
+
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import Detail from './pages/Detail';
+
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="detail" element={<Detail />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
