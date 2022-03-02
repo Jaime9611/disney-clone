f2f284365ab3b050df51594cebd91c1296443f1f
Jaime9611 <jaimeescobar9611@gmail.com>
2022-03-02 07:25:05 -0500

    Added recommended movies section

--- src/components/MoviesPic/MoviesPic.jsx
+++ src/components/MoviesPic/MoviesPic.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
+import { Container, Content, Wrapper } from './MoviesPic.styles';
+
 const MoviesPic = () => {
-  return <div>MoviesPic</div>;
+  return (
+    <Container>
+      <h4>Recommended for You</h4>
+      <Content>
+        <Wrapper>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.miraheze.org%2Fgreatestmovieswiki%2Fthumb%2F0%2F04%2FAvengers2.jpg%2F330px-Avengers2.jpg&f=1&nofb=1" />
+        </Wrapper>
+        <Wrapper>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.miraheze.org%2Fgreatestmovieswiki%2Fthumb%2F0%2F04%2FAvengers2.jpg%2F330px-Avengers2.jpg&f=1&nofb=1" />
+        </Wrapper>
+        <Wrapper>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.miraheze.org%2Fgreatestmovieswiki%2Fthumb%2F0%2F04%2FAvengers2.jpg%2F330px-Avengers2.jpg&f=1&nofb=1" />
+        </Wrapper>
+        <Wrapper>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.miraheze.org%2Fgreatestmovieswiki%2Fthumb%2F0%2F04%2FAvengers2.jpg%2F330px-Avengers2.jpg&f=1&nofb=1" />
+        </Wrapper>
+        <Wrapper>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.miraheze.org%2Fgreatestmovieswiki%2Fthumb%2F0%2F04%2FAvengers2.jpg%2F330px-Avengers2.jpg&f=1&nofb=1" />
+        </Wrapper>
+        <Wrapper>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.miraheze.org%2Fgreatestmovieswiki%2Fthumb%2F0%2F04%2FAvengers2.jpg%2F330px-Avengers2.jpg&f=1&nofb=1" />
+        </Wrapper>
+        <Wrapper>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.miraheze.org%2Fgreatestmovieswiki%2Fthumb%2F0%2F04%2FAvengers2.jpg%2F330px-Avengers2.jpg&f=1&nofb=1" />
+        </Wrapper>
+        <Wrapper>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.miraheze.org%2Fgreatestmovieswiki%2Fthumb%2F0%2F04%2FAvengers2.jpg%2F330px-Avengers2.jpg&f=1&nofb=1" />
+        </Wrapper>
+      </Content>
+    </Container>
+  );
 };
 
 export default MoviesPic;
