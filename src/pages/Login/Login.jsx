3d5221a6d626fd6db1b4366d181cb172701b756c
Jaime9611 <jaimeescobar9611@gmail.com>
2022-03-02 07:56:37 -0500

    Login page layout finished

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-import { Container } from './Login.styles';
+import {
+  Container,
+  CTA,
+  CTALogoOne,
+  CTALogoTwo,
+  SignUp,
+  Description,
+} from './Login.styles';
 
 const Login = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <CTA>
+        <CTALogoOne src="/images/cta-logo-one.svg" />
+        <SignUp>GET ALL THERE</SignUp>
+        <Description>
+          Get premier access to Raya and the Last Dragon for and aditional
+          sucription. As of 01/01/2022, the price of Disney+ will increase $1
+          dollar.
+        </Description>
+        <CTALogoTwo src="/images/cta-logo-two.png" />
+      </CTA>
+    </Container>
+  );
 };
 
 export default Login;
