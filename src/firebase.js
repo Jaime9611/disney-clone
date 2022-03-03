99e9d283d0af037fff439de08abc82cb0cc8b77a
Jaime9611 <jaimeescobar9611@gmail.com>
2022-03-03 16:52:53 -0500

    Instance of firestore

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from 'firebase/app';
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCZ2bhH4qP_hh6uRhrgUcf6X5cESxV4i8I',
+
+  authDomain: 'disney-clone-4b7fc.firebaseapp.com',
+
+  projectId: 'disney-clone-4b7fc',
+
+  storageBucket: 'disney-clone-4b7fc.appspot.com',
+
+  messagingSenderId: '65984615615',
+
+  appId: '1:65984615615:web:f9dd837286ec365fc7e20a',
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
